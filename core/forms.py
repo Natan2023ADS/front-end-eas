832811aadd36ae5d5c339b3d79ec71cba429e2ab
Natan2023ADS <nataniel.neto@hotmail.com>
2025-12-03 15:58:13 -0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Produto
+from .models import Produto, Aluno   # ✅ adicione Aluno aqui
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome','descricao','preco','validade']
+
+
+class AlunoForm(forms.ModelForm):   # ✅ novo formulário
+    class Meta:
+        model = Aluno
+        fields = ['nome', 'sobrenome', 'email']
