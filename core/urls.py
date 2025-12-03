0a7e1d57e13c7d4cf34aafaee5e52a581246d6b1
Natan2023ADS <nataniel.neto@hotmail.com>
2025-12-03 16:12:44 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),            # 
     path('alunos/', views.alunos_list, name='alunos_list'),
+    path('aluno/new/', views.aluno_new, name='aluno_new'),   # ✅ novo
     path('cursos/', views.cursos_list, name='cursos_list'),
     path('produto/new/', views.produto_new, name='produto_new'),
 ]
