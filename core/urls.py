5e53e296c7dce861754ec72bd1b51b76af072893
Natan2023ADS <nataniel.neto@hotmail.com>
2025-12-03 15:53:00 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),            # 
     path('alunos/', views.alunos_list, name='alunos_list'),
     path('cursos/', views.cursos_list, name='cursos_list'),
     path('produto/new/', views.produto_new, name='produto_new'),
